963d6eecdd15dd9ca95d629b00346bc45feff548
joy <leejoys.lab@gmail.com>
2023-08-03 03:42:49 +0900

    feat: 랜덤프로필 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/atoms/Layout/Layout';
 import Chat from './components/pages/ChatPage/ChatPage';
 import Login from './components/pages/LoginPage/LoginPage';
+import Join from './components/pages/JoinPage/JoinPage';
 import Draw from './components/pages/DrawPage/DrawPage';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route path="/" element={<Login />} />
+        <Route path="/join" element={<Join />} />
         <Route path="/game" element={<Draw />} />
         <Route path="/chat" element={<Chat />} />
       </Route>
